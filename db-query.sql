a0d244d2479c8252725e19b8e2e98e23d4392c40
amiryusoff <amir.myusoff@gmail.com>
2022-03-07 15:48:18 +0800

    init

---
+++ db-query.sql
@@ -0,0 +1,107 @@
+CREATE DATABASE training_project
+
+--create table Student_Personal
+CREATE TABLE Student_Personal(
+Std_ID varchar(15) PRIMARY KEY,
+Std_Name varchar(50) NOT NULL,
+Std_DOB date NOT NULL,
+Std_Age int NOT NULL,
+Std_Street varchar(50) NOT NULL,
+Std_State varchar(30) NOT NULL,
+Std_Country varchar(30) DEFAULT 'Malaysia' NOT NULL,
+Std_Postcode int NOT NULL,
+Std_Phone varchar(15) NOT NULL UNIQUE,
+Std_Email varchar(25) NOT NULL UNIQUE,
+CHECK (Std_Age > 0)
+)
+
+--create table Student_Marks
+CREATE TABLE Student_Marks(
+Std_ID varchar(15) FOREIGN KEY REFERENCES Student_Personal(Std_ID) on delete cascade on update cascade,
+Malay int NOT NULL,
+English int NOT NULL,
+Science int NOT NULL,
+Maths int NOT NULL,
+Arts int NOT NULL,
+History int NOT NULL,
+Geography int NOT NULL,
+Total int NOT NULL,
+Average float NOT NULL,
+Result varchar(5) NOT NULL,
+Grade varchar(2) NOT NULL,
+CHECK(Malay > 0 AND Malay <= 100),
+CHECK(English > 0 AND English <= 100),
+CHECK(Science > 0 AND Science <= 100),
+CHECK(Maths > 0 AND Maths <= 100),
+CHECK(Arts > 0 AND Arts <= 100),
+CHECK(History > 0 AND History <= 100),
+CHECK(Geography > 0 AND Geography <= 100),
+CHECK(Result = 'PASS' or Result = 'FAIL')
+)
+
+-- test values
+insert Student_Personal values(101, 'amir', '2022-01-01', 21, 'jalan', 'perak', 'malaysia', 31350, '01251312617', 'amir1@gmail.com')
+insert Student_Marks values(102, 98, 45, 12, 65, 44, 74, 45, 101, 45.5, 'PASS', 'C')
+
+
+--delete selected rows
+DELETE FROM Student_Personal WHERE Std_ID = 102;
+
+select * from Student_Personal
+Select * from Student_Marks
+
+-- bulk import from csv
+GO
+ 
+BULK INSERT dbo.Student_Personal
+FROM 'C:\Users\Amir Yusoff\Documents\GitHub\Python\python-training-miniproject\name-list.csv'
+WITH
+(
+        FORMAT='CSV',
+        FIRSTROW=2
+)
+GO
+ 
+BULK INSERT dbo.Student_Marks
+FROM 'C:\Users\Amir Yusoff\Documents\GitHub\Python\python-training-miniproject\marks-list.csv'
+WITH
+(
+        FORMAT='CSV',
+        FIRSTROW=2
+)
+GO
+
+-- display joined tables
+SELECT * 
+FROM Student_Personal
+LEFT JOIN Student_Marks ON Student_Personal.Std_ID = Student_Marks.Std_ID
+
+SELECT * 
+FROM Student_Marks
+INNER JOIN Student_Personal ON Student_Marks.Std_ID =  Student_Personal.Std_ID
+
+Go
+
+-- update total, average, result, grade
+
+UPDATE Student_Marks SET Total = (Malay + English + Science +  Maths + Arts + History + Geography)
+UPDATE Student_Marks SET Average = Total / 7
+
+UPDATE Student_Marks
+SET Grade = (
+    CASE
+        WHEN Average >= 80 THEN 'A' 
+        WHEN Average >= 70 THEN 'B' 
+		WHEN Average >= 60 THEN 'C' 
+		WHEN Average >= 50 THEN 'D' 
+        ELSE 'F' 
+    END
+)
+
+UPDATE Student_Marks
+SET Result = (
+    CASE
+        WHEN Grade ='A' or Grade ='B' or Grade ='C' or Grade ='D' THEN 'PASS'
+        ELSE 'FAIL' 
+    END
+)
---
+++ main.py
@@ -0,0 +1,234 @@
+import pandas as pd
+import pyodbc
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=AMIR-PC\SQLEXPRESS;'
+                      'Database=training_project;'
+                      'Trusted_Connection=yes;')
+cursor = conn.cursor()
+
+print('Connected to SQL Server')
+
+
+#function for option 1 | add student info
+def addStudent():
+    id = input("Enter Student ID: ")
+    name = input("Enter Student Name: ")
+    dob = input("Enter Student DOB: ")
+    age = int(input("Enter Student Age: "))
+    street = input("Enter Student Street: ")
+    state = input("Enter Student State: ")
+    country = input("Enter Student Country: ")
+    postcode = int(input("Enter Student Postcode: "))
+    phone = input("Enter Student Phone: ")
+    email = input("Enter Student Email: ")
+
+    cursor.execute("""
+        INSERT INTO 
+            dbo.Student_Personal 
+        VALUES 
+            (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (id, name, dob, age, street, state, country, postcode, phone, email))
+
+    conn.commit()
+    print('Student information successfully added\n')
+
+#function for option 2 | insert student marks
+def addMarks():
+    id = input("Enter Student ID: ")
+    malay = int(input("Enter marks for Malay:"))
+    english = int(input("Enter marks for English:"))
+    science = int(input("Enter marks for Science:"))
+    maths = int(input("Enter marks for Maths:"))
+    arts = int(input("Enter marks for Arts:"))
+    history = int(input("Enter marks for History:"))
+    geography = int(input("Enter marks for Geography:"))
+
+    total = malay + english + science + maths + arts + history + geography
+    average = total / 7
+
+    if average >= 80:
+        result = 'PASS'
+        grade = 'A'
+    elif average >= 70:
+        result = 'PASS'
+        grade = 'B'  
+    elif average >= 60:
+        result = 'PASS'
+        grade = 'C'
+    elif average >= 50:
+        result = 'PASS'
+        grade = 'D'
+    else:
+        result = 'FAIL'
+        grade = 'F'
+
+    cursor.execute("""
+        INSERT INTO 
+            dbo.Student_Marks 
+        VALUES 
+            (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (id, malay, english, science, maths, arts, history, geography, total, average, result, grade))
+
+    conn.commit()
+    print('Student marks successfully added\n')
+
+#function for option 3 | update student info
+def updateStudent():
+    id = int(input("Enter Student ID to update: "))
+
+    print('What to update?')
+    print('1. Student Name')
+    print('2. Student DOB')
+    print('3. Student Age')
+    print('4. Student Street')
+    print('5. Student State')
+    print('6. Student Country')
+    print('7. Student Postcode')
+    print('8. Student Phone')
+    print('9. Student Email\n')
+
+    menu = int(input('Enter your choice: '))
+
+    if menu == 1:
+        update = input("Enter Student Name: ")
+        cursor.execute("UPDATE dbo.Student_Personal SET Std_Name = '%s' WHERE Std_ID = %d " %(update, id))
+    elif menu == 2:
+        update = input("Enter Student DOB: ")
+        cursor.execute("UPDATE dbo.Student_Personal SET Std_DOB = '%s' WHERE Std_ID = %d " %(update, id))
+    elif menu == 3:
+        update = int(input("Enter Student Age: "))
+        cursor.execute("UPDATE dbo.Student_Personal SET Std_Age = %d WHERE Std_ID = %d " %(update, id))
+    elif menu == 4:
+        update = input("Enter Student Street: ")
+        cursor.execute("UPDATE dbo.Student_Personal SET Std_Street = '%s' WHERE Std_ID = %d " %(update, id))
+    elif menu == 5:
+        update = input("Enter Student State: ")
+        cursor.execute("UPDATE dbo.Student_Personal SET Std_State = '%s' WHERE Std_ID = %d " %(update, id))
+    elif menu == 6:
+        update = input("Enter Student Country: ")
+        cursor.execute("UPDATE dbo.Student_Personal SET Std_Country = '%s' WHERE Std_ID = %d " %(update, id))
+    elif menu == 7:
+        update = int(input("Enter Student Postcode: "))
+        cursor.execute("UPDATE dbo.Student_Personal SET Std_Postcode = %d WHERE Std_ID = %d " %(update, id))
+    elif menu == 8:
+        update = input("Enter Student Phone: ")
+        cursor.execute("UPDATE dbo.Student_Personal SET Std_Phone = '%s' WHERE Std_ID = %d " %(update, id))
+    elif menu == 9:
+        update = input("Enter Student Email: ")
+        cursor.execute("UPDATE dbo.Student_Personal SET Std_Email = '%s' WHERE Std_ID = %d " %(update, id))
+    else:
+        print('Invalid input')
+    
+    conn.commit()
+    print('Student information successfully updated\n')
+
+#function for option 4 | delete student info
+def deleteStudent():
+    id = int(input("Enter Student ID to delete: "))
+    cursor.execute("DELETE FROM dbo.Student_Personal WHERE Std_ID = %d " %(id))
+    conn.commit()
+    print('Student information successfully deleted\n')
+
+#function for option 5 | search student info
+def searchStudent():
+    id = int(input("Enter Student ID to search: "))
+
+    cursor.execute("SELECT * FROM dbo.Student_Personal WHERE Std_ID = %d " %(id))
+    row = cursor.fetchone()
+
+    if row == None:
+        print('Student not found')
+    else:
+        print('Student found')
+        print('Student ID: ', row[0])
+        print('Student Name: ', row[1])
+        print('Student DOB: ', row[2])
+        print('Student Age: ', row[3])
+        print('Student Street: ', row[4])
+        print('Student State: ', row[5])
+        print('Student Country: ', row[6])
+        print('Student Postcode: ', row[7])
+        print('Student Phone: ', row[8])
+        print('Student Email: ', row[9])
+
+    conn.commit()
+
+#function for option 6 | display all student report
+def displayReport():
+    cursor.execute('''
+                SELECT * 
+                FROM Student_Personal
+                LEFT JOIN Student_Marks ON Student_Personal.Std_ID = Student_Marks.Std_ID
+                ''')
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print('Student ID: ', row[0])
+        print('Student Name: ', row[1])
+        print('Student DOB: ', row[2])
+        print('Student Age: ', row[3])
+        print('Student Street: ', row[4])
+        print('Student State: ', row[5])
+        print('Student Country: ', row[6])
+        print('Student Postcode: ', row[7])
+        print('Student Phone: ', row[8])
+        print('Student Email: ', row[9])
+        print('\n-------------------------------')
+        print('-----------MARKS-------------')
+        print('Student Malay Marks: ', row[10])
+        print('Student English Marks: ', row[11])
+        print('Student Science Marks: ', row[12])
+        print('Student Maths Marks: ', row[13])
+        print('Student Arts Marks: ', row[14])
+        print('Student History Marks: ', row[15])
+        print('Student Geography Marks: ', row[16])
+        print('\n')
+
+    conn.commit()
+
+
+# main program
+condition = 1
+
+print('STUDENT MANAGEMENT SYSTEM\n')
+
+print('1. Add a new student information')
+print('2. Insert student marks')
+print('3. Update student information')
+print('4. Delete student information')
+print('5. Search student information')
+print('6. Display all student report')
+print('0. Exit')
+print('\n')
+
+choice = int(input('Enter your choice: '))
+
+while (condition):
+    if (choice == 1):
+        addStudent()
+    elif (choice == 2):
+        addMarks()
+    elif (choice == 3):
+        updateStudent()
+    elif (choice == 4):
+        deleteStudent()
+    elif (choice == 5):
+        searchStudent()
+    elif (choice == 6):
+        displayReport()
+    elif (choice == 0):
+        condition = 0
+        print('Exiting...')
+        break
+
+    choice = int(input('Enter your choice: '))    
+
+
+
+
+
+
+
+
+
